842cd159185cf4e4f193be2cdf3c5df2ec5306aa
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2025-11-08 15:10:14 +0100

    another day of spring security

---
+++ src/main/java/com/example/springSec/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springSec;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public  String Greet(HttpServletRequest request){
+        return ("welcome to spring security" + request.getSession().getId());
+    }
+}
---
+++ src/main/java/com/example/springSec/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.springSec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.csrf(csrf -> csrf.disable());
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+//        Customizer<CsrfConfigurer<Htt>>
+//       return http.build();
+
+        http.authorizeHttpRequests()
+        Customizer <CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> cust) {
+                cust.disable();
+            }
+        };
+        http.csrf(custCsrf);
+    }
+}
+
